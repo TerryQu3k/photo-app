0f816b58618c441e9807c27515bca32e5f66fa38
Terry <terryquek95@gmail.com>
2016-06-30 17:08:34 +0800

    remove turbolink and fix sign up bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -23,7 +23,7 @@ class RegistrationsController < Devise::RegistrationsController
 				end
 
 				if resource.active_for_authentication?
-			       set_flash_message! :notice, :signed_up if is_flashing_format?
+			       set_flash_message :notice, :signed_up if is_flashing_format?
 			       sign_up(resource_name, resource)
 			       respond_with resource, location: after_sign_up_path_for(resource)
 			    else
